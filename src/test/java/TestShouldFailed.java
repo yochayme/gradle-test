02eda9da121507290eda1d9df252edb247472716
menyo01 <yochay.menachem@ca.com>
2018-01-01 08:12:32 +0200

    ss

---
+++ src/test/java/TestShouldFailed.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by menyo01 on 01/01/2018.
+ */
+public class TestShouldFailed {
+
+
+
+    @Test
+    public void testShouldFailed() {
+        assertEquals("test1", "test");
+    }
+}
---
+++ src/test/java/TestStringsShouldPass.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by menyo01 on 01/01/2018.
+ */
+public class TestStringsShouldPass {
+
+    @Test
+    public void testShouldPass() {
+        assertEquals("test", "test");
+    }
+
+}
